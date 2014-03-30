8c7bc1c8434558cd71d102562aa11975f9ec616d
unknown <Karl-Henrik>
2014-03-30 16:25:51 +0200

    Added software to make remote installs

---
+++ DevSoftware.ps1
@@ -0,0 +1,24 @@
+# Visual studio + add-ins
+cinst VisualStudio2013Ultimate -InstallArguments "Blend LightSwitch VC_MFC_Libraries OfficeDeveloperTools SQL WebTools Win8SDK SilverLight_Developer_Kit WindowsPhone80"
+cinst xUnit.vs2013
+cinst WebEssentials2013
+cinst resharper
+
+# databases and tools
+cinst MsSqlServer2012Express
+
+# browsers and add-ins
+cinst Firefox
+cinst adblockplusfirefox
+
+#Sourcecode handlers
+cinst git.install
+cinst gitextensions
+
+# Other tools
+cinst notepadplusplus
+cinst 7zip
+cinst fiddler
+cinst curl
+cinst linqpad4
+
---
+++ install.bat
@@ -0,0 +1,7 @@
+# INSTALL CHOCOLATERY
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+
+
+# INSTALL APPS AND DEV TOOLS
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://github.com/karl-henrik/Installationscripts/DevSoftware.ps1'))"
+
