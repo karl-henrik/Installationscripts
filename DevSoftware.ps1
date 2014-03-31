e03285880668b67697b5f1b09e10873a746addf5
Karl-Henrik Nilsson <kh@mean.se>
2014-03-31 13:20:00 +0200

    fixed typo
    fixed type errors in line 2# cinst WebEssentials2012

--- DevSoftware.ps1
+++ DevSoftware.ps1
@@ -1,7 +1,6 @@
 # Visual studio + add-ins
 cinst VisualStudio2013Ultimate -InstallArguments "Blend LightSwitch VC_MFC_Libraries OfficeDeveloperTools SQL WebTools Win8SDK SilverLight_Developer_Kit WindowsPhone80"
-cinst xUnit.vs2013
-cinst WebEssentials2013
+cinst WebEssentials2012 
 cinst resharper
 
 # databases and tools
