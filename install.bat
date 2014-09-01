ab0b675a32b5157838d12a8597ba0025e31a0c94
Karl-Henrik Nilsson <kh@mean.se>
2014-09-01 07:41:36 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 # INSTALL CHOCOLATERY
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+#@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
 
 
 # INSTALL APPS AND DEV TOOLS
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://github.com/karl-henrik/Installationscripts/DevSoftware.ps1'))"
-
+#@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://github.com/karl-henrik/Installationscripts/DevSoftware.ps1'))"
+echo test works
