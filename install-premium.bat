a900f7173403c51db3c9ede7ecc370c99006eea6
Karl-Henrik Nilsson <kh@mean.se>
2014-09-01 10:59:57 +0200

    Update install-premium.bat

--- install-premium.bat
+++ install-premium.bat
@@ -1,8 +1,8 @@
 # INSTALL CHOCOLATERY
 echo "Installing Chocolatey Nuget Pkg-manager"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+iex ((new-object net.webclient).DownloadString('http://chocolatey.org/install.ps1'))
+SET PATH=%PATH%;%systemdrive%\chocolatey\bin
 
 # INSTALL APPS AND DEV TOOLS
 echo "Installing apps and dev tools"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://github.com/karl-henrik/Installationscripts/DevSoftware-Premium.ps1'))"
-
+iex ((new-object net.webclient).DownloadString('https://github.com/karl-henrik/Installationscripts/DevSoftware-Premium.ps1'))
